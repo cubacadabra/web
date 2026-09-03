bc2ec56edbcea0e049869d17a9c750ee0bda8e49
aa <oneone@gmail.com>
2026-09-03 05:57:07 -0700

    terms

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
       input: {
         main: new URL("./index.html", import.meta.url).pathname,
         about: new URL("./about/index.html", import.meta.url).pathname,
+        terms: new URL("./terms/index.html", import.meta.url).pathname,
+        privacy: new URL("./privacy/index.html", import.meta.url).pathname,
       },
     },
   },
